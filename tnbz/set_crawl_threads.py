66cc0763c181758ec79c67e1d0af7d7695e7a2cd
xxyLiang <651133439@qq.com>
2021-10-08 19:11:59 +0800

    Update set_crawl_threads.py

--- tnbz/set_crawl_threads.py
+++ tnbz/set_crawl_threads.py
@@ -1,11 +1,12 @@
 import pymysql
 import traceback
 
+
 db = pymysql.connect(host="localhost", user="root", password="651133439a", database="tmjy", charset='utf8mb4')
 cursor = db.cursor()
 sql = "update `threads` set will_crawl = 1 where author_id=%s and (TO_DAYS(publish_date) - TO_DAYS(%s) <= 30)"
 
-
+'''
 try:
     cursor.execute("select author_id, publish_date from threads where new_user=1")
     ts = cursor.fetchall()
@@ -18,3 +19,21 @@ for t in ts:
     except:
         traceback.print_exc()
         db.rollback()
+'''
+
+
+# 设置每个post回复的文本，默认为首帖文本
+try:
+    cursor.execute("select pid, tid from test where initiate=0")
+    rs = cursor.fetchall()
+except:
+    pass
+
+for r in rs:
+    try:
+        cursor.execute("update test set initiate_content=(select content from posts where tid=%s and is_initiate_post=1) where pid=%s", (r[1], r[0]))
+        db.commit()
+    except:
+        traceback.print_exc()
+        db.rollback()
+
