a68c4e5a7858bd77d982c07ef4f62ecc4b5151ce
xxyLiang <651133439@qq.com>
2021-10-12 23:20:13 +0800

    commit

--- tnbz/test.py
+++ tnbz/test.py
@@ -1,24 +1,18 @@
-import requests
+import sys
 import time
 
-url='https://2021.ip138.com/'
-proxyaddr = ""    #代理IP地址
-proxyport = 57114               #代理IP端口
-proxyusernm = "651133439"        #代理帐号
-proxypasswd = "4201119!Dly"        #代理密码
-#name = input();
-proxyurl="http://"+proxyusernm+":"+proxypasswd+"@"+proxyaddr+":"+"%d"%proxyport
+words = []
+with open('./material/NTUSD_positive_simplified.txt', 'r', encoding='utf-16') as f:
+    for w in f.readlines():
+        flag = 0
+        if '.' in w:
+            continue
+        for i in words:
+            if i in w:
+                flag = 1
+        if flag == 0:
+            words.append(w.strip('\n'))
 
-t1 = time.time()
-r = requests.get(url,proxies={'http':proxyurl,'https':proxyurl},headers={
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate",
-    "Accept-Language":"zh-CN,zh;q=0.9",
-    "Cache-Control":"max-age=0",
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"})
-r.encoding='gb2312'
-
-t2 = time.time()
-
-print(r.text)
-print("时间差:" , (t2 - t1));
+with open('./material/pos.txt', 'w', encoding='utf-16') as f:
+    for w in words:
+        f.write(w + '\n')
--- tnbz/train.py
+++ tnbz/train.py
@@ -1,2 +1,62 @@
-from feature import joint_feature
+from extract_feature import Feature
+import numpy as np
+import os
+import pandas as pd
+import pymysql
 
+
+def check_sentences(results):
+    new_results = []
+    for r in results:
+        if r[4] == '' or r[4] == ' ':  # 空内容，删掉
+            continue
+        temp = list(r)  # 转list，以便将过长的句子截短
+        if len(temp[4]) > 3000:
+            temp[4] = temp[4][:3000]
+        new_results.append(temp)
+    return new_results
+
+
+def get_train_vector(batch_size=16):
+    # if vector file can not found:
+    # load raw train data from database, transform it to vector/features, and store to local disk (DataFrame).
+    print("Start to transform train_data to vector.")
+    transformer = Feature()
+    train_vector = pd.DataFrame()
+    db = pymysql.connect(host="localhost", user="root", password="651133439a", database="tmjy", charset='utf8mb4')
+    cursor = db.cursor()
+    total_num = cursor.execute("select * from `train_table`")
+    count = 0
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if len(batch) == 0:
+            break
+        df = pd.DataFrame(check_sentences(batch), columns=['pid', 'is_init', 'is_reply', 'img_num', 'content',
+                                                           'reply_content', 'SIS', 'PIS', 'SES', 'PES', 'COM'])
+        sentences = df.iloc[:, 4]
+        feature = transformer.joint_feature(list(sentences))
+        init_reply_img = df.iloc[:, 1:4]
+        tags = df.iloc[:, -5:]
+        vector = pd.concat([feature, init_reply_img, tags], axis=1)
+        train_vector = train_vector.append(vector)
+        count += len(batch)
+        print('\r[%s%s] %s / %s' %
+              (('#' * int(50 * count / total_num)), ' ' * (50 - int(50 * count / total_num)), count, total_num),
+              end='')
+    train_vector.to_pickle('./data/train_vector.df')
+    db.close()
+    print("\nTransform Vector Complete.")
+    return train_vector
+
+
+def run(force_trans=0):
+    if os.path.exists('./data/train_vector.df') and not force_trans:
+        df = pd.read_pickle('./data/train_vector.df')
+    else:
+        df = get_train_vector()
+    feature = df.iloc[:, :-5]
+    tags = df.iloc[:, -5:]
+
+
+if __name__ == '__main__':
+    run(1)
