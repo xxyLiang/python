dabc65e0708bba54864177eec1b68ba3da24f747
xxyLiang <651133439@qq.com>
2022-02-15 22:01:51 +0800

    Update str.py

--- other/str.py
+++ other/str.py
@@ -53,6 +53,34 @@ def max_reverse(s):
         
     return res
 
+def maxUniqueLength(arr) -> int:
+        # write code here
+        longest_length,i = 0,-1
+        arr_length = len(arr)
+        l_str = {}      # 出现过的字符及其最后出现的位置
+        for j in range(arr_length):         # i左指针，j右指针
+            if arr[j] in l_str and l_str[arr[j]] > i:       # 当前字符在l_str中出现，且最后出现在i~j范围内，才算重复
+                i = l_str[arr[j]]
+            l_str[arr[j]] = j                               # 记录当前字符最后出现位置
+            longest_length = max(longest_length, j-i)
+        return max(longest_length, j-i)
+
+
+def maxSameString(s):       # 返回最长重复子字符串
+    last = ''
+    leng = maxlen = start = 0       # 由于要返回字符串，还需记录字符串开始位置
+    for i in range(len(s)):
+        if s[i] == last:            # 如果相同，则继续增加
+            leng += 1
+        else:
+            if leng > maxlen:       # 如果刷新了长度，更新maxlen，更新最长字符串开始位置为i-leng
+                maxlen = leng 
+                start = i-leng             
+            last = s[i]
+            leng = 1
+    return s[start:start+maxlen]
+
+
 
 if __name__ == '__main__':
     print(max_reverse('ddbcdddccabbdcdbdbccdaabcdaaaabaadcbadaabdcbaccdabdbccacdddcdcaacdadbacbbccabdabdcddbaacbadacdadbaccbcaadddddddabddbabdaaacdddcd'))
